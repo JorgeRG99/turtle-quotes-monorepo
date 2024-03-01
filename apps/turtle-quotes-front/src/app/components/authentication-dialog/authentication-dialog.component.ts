2b14343a14d80f3fc07ffd08c3de5e64bb845292
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-03-01 10:34:10 +0100

    created demo form for authentication

--- apps/turtle-quotes-front/src/app/components/authentication-dialog/authentication-dialog.component.ts
+++ apps/turtle-quotes-front/src/app/components/authentication-dialog/authentication-dialog.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogService } from '../../services/dialog/dialog.service';
 import { Observable } from 'rxjs';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './authentication-dialog.component.html',
   styleUrl: './authentication-dialog.component.css',
 })
@@ -14,7 +15,13 @@ export class AuthenticationDialogComponent {
   dialogService = inject(DialogService);
   $selectedForm!: Observable<string>;
 
-  constructor() {
+  authForm = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    password: ['']
+  });
+
+  constructor(private formBuilder: FormBuilder) {
     this.$selectedForm = this.dialogService.$selectedForm.getSelectedForm();
   }
 
@@ -26,5 +33,15 @@ export class AuthenticationDialogComponent {
   changeSelectedForm(selectedForm: string, e: Event) {
     e.preventDefault();
     this.dialogService.$selectedForm.setSelectedForm(selectedForm);
+  }
+
+  onSubmit() {
+    fetch('http://localhost:3000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.authForm.value)
+    })
   }
 }
